104d9dcc624d4372c17453fa6ba0eab104d8fd32
Pablo Sánchez <pablo.sanchez@aranova.es>
2025-09-17 15:25:05 +0200

    fix

---
+++ src/aranet-2025/src/app/(pages)/invoice/list/page.tsx
@@ -0,0 +1,46 @@
+import { MenuItem, NotAuthorized, TopBreadcrumb } from "@aranova/aranova-react-ui";
+import ServerDataPlain from "@/app/data/ServerDataPlain";
+import { getSession } from "@/app/lib/session";
+import { PageStoreHeader } from "@/app/components";
+import { Metadata } from "next";
+
+export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'Facturas - Finanzas',
+};
+
+export default async function InvoiceListPage() {
+  const session = await getSession();
+  if (!session) {
+    return <NotAuthorized />;
+  }
+
+  const dataPlain = ServerDataPlain.getInstance();
+  const me = await dataPlain.useMe(session.id);
+  if (!me.data?.id) {
+    return <NotAuthorized />;
+  }
+  
+  const links: MenuItem[] = [
+    { name: 'Inicio', href: '/' },
+    { name: 'Finanzas', href: null },
+    { name: 'Facturas', href: '' },
+  ];
+
+  return (
+    <>
+      <TopBreadcrumb links={links} />
+      <div className="flex flex-col flex-1">
+        <PageStoreHeader
+          title="Facturas"
+          subtitle="Listado de facturas"
+          model="invoice"
+          add_button_text="Añadir factura"
+          search_placeholder="Buscar facturas..."
+        />
+      </div>
+    </>
+  )
+
+}
--- src/aranet-2025/src/app/components/shell/PageStoreHeader.tsx
+++ src/aranet-2025/src/app/components/shell/PageStoreHeader.tsx
@@ -10,22 +10,38 @@ interface Props {
   state?: number;
   subtitle?: string;
   model: string;
+  search_placeholder?: string;
   nb_items?: number;
   search_value?: string | string[];
   main_button?: React.ReactNode;
+  add_button_text?: string;
+  add_button_href?: string;
+  add_button_icon?: React.ReactNode;
 }
 
 
 export const PageStoreHeader = ({
   main_button,
+  add_button_href,
+  add_button_text,
   state,
   ...props
 }: Props) => {
-  const { setSearchTerm } = useFormUiStore();
+  const { setSearchTerm, openDrawer } = useFormUiStore();
   const { getSearchInputValue, setSearchInputValue, dumpUrl } = useSearchStore();
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
+  const add_props: Record<string, string | undefined | null | Fn> = {
+    add_button_text,
+    add_button_href,
+    add_button_click: null,
+  };
+
+  if (!add_button_href && add_button_text) {
+    add_props.add_button_click = () => openDrawer({});
+  }
 
   //Funcion que se ejecuta al hacer click en buscar
   const handleSearch = useCallback(
@@ -58,6 +74,7 @@ export const PageStoreHeader = ({
         searchFn={handleSearch}
         state={state}
         main_button={main_button}
+        {...add_props}
       />
     </>
   );
